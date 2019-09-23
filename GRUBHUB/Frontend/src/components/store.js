8737f62d1613c8d03ee74dfe9d00fa0dabbe4433
Naga Abhilash Reddy Julakanti <nagaabhilash66@gmail.com>
2019-09-23 11:36:08 -0700

    Delete store.js

--- GRUBHUB/Frontend/src/components/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore} from 'redux';
-import AllReducers from '../reducers/AllReducers'
-
-const store = createStore(
-    AllReducers,
-    window.devToolsExtension && window.devToolsExtension()
-)
-
-export default store;
