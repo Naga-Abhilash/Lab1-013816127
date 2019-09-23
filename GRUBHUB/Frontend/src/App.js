3bb5523976fe886f1b864383fbe0f7e076c51503
Naga Abhilash Reddy Julakanti <nagaabhilashreddy.julakanti@sjsu.edu>
2019-09-23 11:23:33 -0700

    Add files via upload
    updated front end

---
+++ GRUBHUB/Frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/main'
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/styles.css'
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <header >
+          <BrowserRouter>
+            <div>
+              <Main />
+            </div>
+          </BrowserRouter>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
